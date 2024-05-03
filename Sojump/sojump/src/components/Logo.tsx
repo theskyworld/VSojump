03249313b708ef6dac1c5cbb5fa8f0c239824064
theskyworld <2456450687@qq.com>
2024-05-03 18:02:33 +0800

    refactor: add Background.tsx, change style of HomePage

--- Sojump/sojump/src/components/Logo.tsx
+++ Sojump/sojump/src/components/Logo.tsx
@@ -9,14 +9,13 @@ const { Title } = Typography;
 
 
 const Logo: FC = () => {
-    return(
+    return (
         <div className={styles.container}>
             <Link to="/">
                 <Space>
                     {/* Title的level属性可以指定级别，默认值为1，等价于h1 */}
-                    <Title>
-                    </Title>
-                    <Title>V问卷</Title>
+                    <img width="30px" height="30px" src="/logo.jpg" alt="" />
+                    <Title className={styles.text}>创意智库</Title>
                 </Space>
             </Link>
         </div>
--- Sojump/sojump/src/pages/Home.tsx
+++ Sojump/sojump/src/pages/Home.tsx
@@ -3,6 +3,7 @@ import { useNavigate, Link } from "react-router-dom";
 import { Button, Typography } from "antd";
 import { MANAGE_LIST_URL, USERNAME_KEY } from "../assets/ts/constants";
 import styles from "./Home.module.scss";
+import Background from "../components/background/Background";
 
 
 const { Title, Paragraph } = Typography;
@@ -12,19 +13,21 @@ const Home: FC = () => {
     const nav = useNavigate();
     useEffect(() => {
         if (!localStorage.getItem(USERNAME_KEY)) {
-            localStorage.setItem(USERNAME_KEY, JSON.stringify({username:"",password:""}));
+            localStorage.setItem(USERNAME_KEY, JSON.stringify({ username: "", password: "" }));
         }
-    },[])
+    }, [])
 
     return (
         <div className={styles.container}>
+            <div className={styles.bg}>
+                <Background />
+            </div>
             <div className={styles.info}>
-                <Title>问卷调查 | 在线投票</Title>
+                <Title>创意工厂|快速创建问卷</Title>
                 <Paragraph>已累计创建问卷100份，发布问卷50份，收到答卷100份</Paragraph>
                 <div>
                     <Button type="primary" onClick={() => nav(MANAGE_LIST_URL)}>开始使用</Button>
                 </div>
-
             </div>
         </div>
     )
